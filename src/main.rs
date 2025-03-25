13a7c097503eb19a745bfac1f68a06dc5a3d380a
Atharv Rawal <atharvrawal04@gmail.com>
2025-03-25 10:49:46 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-use tokio::net::{TcpListener, TcpStream}; // TcpListner listens for incoming connections
-use tokio::io::{AsyncReadExt, AsyncWriteExt}; // Provides async read/write function
-
-async fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    if let Ok(size) = stream.read(&mut buffer).await {
-        println!("Received: {}", String::from_utf8_lossy(&buffer[..size]));
-        let _ = stream.write_all(b"Hello from server").await;
-    }
-}
-
-#[tokio::main]
-async fn main() -> tokio::io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080").await?;
-    println!("Server listening on port 8080...");
-
-    loop {
-        let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            handle_client(socket).await;
-        });
-    }
+fn main(){
+    print!("hello");
 }
